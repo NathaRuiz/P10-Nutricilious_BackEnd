be70fc9b040d87610a5e508b1e45496e6968d9c2
Desiree <desireesanchezdev@gmail.com>
2024-03-06 11:46:14 +0100

    Created routes roles on api.php

--- routes/api.php
+++ routes/api.php
@@ -69,5 +69,15 @@ Route::middleware(['auth:sanctum', 'Company'])->group(function () {
     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
 
+Route::middleware(['auth:sanctum', 'User'])->group(function () {
+   
+});
 
+Route::middleware(['auth:sanctum', 'Admin'])->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
